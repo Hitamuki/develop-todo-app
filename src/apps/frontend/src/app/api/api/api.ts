a5c979eda3e72645b2ff9ea312b9760f3c0bc9cc
Hitamuki <hitamuki024@gmail.com>
2024-11-19 23:39:15 +0900

    OpenAPI Generator実行

---
+++ src/apps/frontend/src/app/api/api/api.ts
@@ -0,0 +1,3 @@
+export * from './tasks.service';
+import { TasksService } from './tasks.service';
+export const APIS = [TasksService];
---
+++ src/apps/frontend/src/app/api/model/models.ts
@@ -0,0 +1,5 @@
+export * from './model-error';
+export * from './task';
+export * from './task-create';
+export * from './tasks-task-id-get400-response';
+export * from './tasks-task-id-get404-response';
