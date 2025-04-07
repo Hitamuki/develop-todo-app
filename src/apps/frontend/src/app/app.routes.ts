02ccd341f0cdee059c802b479d4394183eefb7a9
Hitamuki <hitamuki024@gmail.com>
2025-04-07 22:10:21 +0900

    遅延読み込みでルーティング設定

--- src/apps/frontend/src/app/app.routes.ts
+++ src/apps/frontend/src/app/app.routes.ts
@@ -1,6 +1,16 @@
 import type { Routes } from '@angular/router';
 
 /**
- *
+ * アプリケーションのルート定義
  */
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/task-list/task-list.component').then((m) => m.TaskListComponent),
+  },
+];
